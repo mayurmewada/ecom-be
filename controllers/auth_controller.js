99df864be9efebfe7c393dbda9823e0f2cb6922d
mayurmewada <mayurmewada00@gmail.com>
2025-03-19 23:58:54 +0530

    feat: login api

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -1,6 +1,34 @@
 const bcrypt = require("bcryptjs");
 const authModel = require("../schema/auth_schema");
 
+const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const isUserFound = await authModel.findOne({ email });
+        if (!isUserFound) {
+            return res.status(400).json({
+                message: "Invalid credentials",
+            });
+        }
+        const genuineUser = bcrypt.compareSync(password, isUserFound.password);
+        if (!genuineUser) {
+            return res.status(400).json({
+                message: "Invalid credentials",
+            });
+        }
+        return res.status(200).json({
+            data: { email: isUserFound.email, _id: isUserFound._id },
+            message: "User Loggedin Successfully",
+        });
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "Internal Server Error",
+            error: error.message || "Something went wrong",
+        });
+    }
+};
+
 const signup = async (req, res) => {
     try {
         const { email, phone, password } = req.body;
@@ -23,4 +51,4 @@ const signup = async (req, res) => {
     }
 };
 
-module.exports = { signup };
+module.exports = { login, signup };
--- routes/auth_route.js
+++ routes/auth_route.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const { signup } = require("../controllers/auth_controller");
+const { signup, login } = require("../controllers/auth_controller");
 
 const route = express.Router();
 
-route.post("/login");
+route.post("/login", login);
 route.post("/signup", signup);
 route.post("/forgot-password");
 
